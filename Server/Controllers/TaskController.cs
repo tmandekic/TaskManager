22abb79cbe9afb8a4802eba38c8614cfd89e89c1
Kristina <tmandekic@hotmail.com>
2021-07-22 16:56:46 -0400

    Add project files.

---
+++ Server/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using TaskManager.Shared;
+
+namespace TaskManager.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ILogger<TaskController> _logger;
+
+        private static readonly List<TaskItem> Tasks = new List<TaskItem>()
+        {
+            new TaskItem{Id=1,TaskName="task 1", DueBy=new DateTime(2021,01,01), IsCompleted=true},
+            new TaskItem{Id=2,TaskName="task 2", DueBy=new DateTime(2021,08,04)},
+            new TaskItem{Id=3,TaskName = "task 3", DueBy=new DateTime(2021,07,20), IsCompleted=false}
+        };
+
+        public TaskController(ILogger<TaskController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TaskItem> Get()
+        {
+            return Tasks;
+        }
+    }
+}
---
+++ Shared/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Shared
+{
+    public class TaskItem
+    {
+        [Required(ErrorMessage ="Please enter task name")]
+        public string TaskName { get; set; }
+        
+        [Required(ErrorMessage ="Please enter date that the task is due by")]
+        [DataType(DataType.Date, ErrorMessage ="Please enter valid date")]
+        public DateTime DueBy { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public bool IsEditing { get; set; }
+        public int Id { get; set; }
+
+    }
+}
